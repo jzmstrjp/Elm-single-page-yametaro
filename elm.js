56dfeb6be519d445ea456670c87884c7fb537f6f
Takashi Tokumoto <jazzm@ster.jp.net>
2019-06-24 00:47:27 +0900

    make

--- elm.js
+++ elm.js
@@ -6107,7 +6107,7 @@ var author$project$Main$view = function (model) {
 									]),
 								_List_fromArray(
 									[
-										elm$html$Html$text('Elm SPA Demo')
+										elm$html$Html$text('Elm SPA Demo Site')
 									]))
 							])),
 						A2(
